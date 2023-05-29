af22f2e8fb6b0398a584b42ddbd4938af96f1982
Tnluser <tnluser@Abhinavs-Mac-Pro.local>
2023-05-29 21:41:10 +0530

    Filter expenses basis dropdown

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "Car Insurance",
+    amount: 146.43,
+    date: new Date(2021, 2, 7),
+  },
+  {
+    id: "e3",
+    title: "Kitchen Towel",
+    amount: 4.94,
+    date: new Date(2022, 7, 12),
+  },
+  { id: "e4", title: "Spices", amount: 66.54, date: new Date(2022, 8, 11) },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    {
-      id: "e2",
-      title: "Car Insurance",
-      amount: 146.43,
-      date: new Date(2021, 2, 7),
-    },
-    {
-      id: "e3",
-      title: "Kitchen Towel",
-      amount: 4.94,
-      date: new Date(2022, 7, 12),
-    },
-    { id: "e4", title: "Spices", amount: 66.54, date: new Date(2022, 8, 11) },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
